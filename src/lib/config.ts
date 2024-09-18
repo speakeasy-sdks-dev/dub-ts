af3ff26018515f4441b0747814762e21b1e76fa8
regression-test-executor <regression-test-executor@speakeasy.com>
2024-09-18 11:01:15 +0000

    openapi-generation@49d96979901c5e8afe39a135cfbabed6eeddfcc9

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -58,6 +58,6 @@ export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "0.0.1",
   sdkVersion: "0.40.1",
-  genVersion: "2.415.8",
-  userAgent: "speakeasy-sdk/typescript 0.40.1 2.415.8 0.0.1 dub",
+  genVersion: "2.416.4",
+  userAgent: "speakeasy-sdk/typescript 0.40.1 2.416.4 0.0.1 dub",
 } as const;
